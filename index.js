fe262d89af2c741bfe2418a5f9ba6360c7fb2246
Hao Lam <tuonghao2001@gmail.com>
2023-07-12 09:54:38 -0700

    using slugify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 // const hello = "Hello World";
@@ -46,7 +47,8 @@ const productTemplate = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 const server = http.createServer((req, res) => {
   // console.log(req);
   // res.end("Hello from the server!");
